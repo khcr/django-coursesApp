e44eb42b1f8cc048b68ffecb31eb1e93ec861c74
khcr <kocher.ke@gmail.com>
2015-02-16 13:04:05 +0100

    support maths

--- courses/views.py
+++ courses/views.py
@@ -14,9 +14,9 @@ def pdf(request, pk):
     # https://www.ampad.de/blog/generating-pdfs-django/
     course = Course.objects.get(id=pk)
     template = get_template("courses/pdf.html")
-    context = {"course": course}
-    html = template.render(RequestContext(request, context))
-    html = re.sub(r"\|{2}(?P<content>[^|]*)\|{2}", "<div class='math'>\g<content></div>", html)
+    html = template.render(RequestContext(request, {"course": course}))
+    # replace maths expressions
+    html = re.sub(r"\|{2}(?P<content>[^|]*)\|{2}", "<div class='math'><img src='http://latex.codecogs.com/gif.latex?\g<content>'></div>", html)
     response = HttpResponse(content_type="application/pdf")
-    HTML(string=html).write_pdf(response)
+    HTML(string=html, base_url=request.build_absolute_uri()).write_pdf(response)
     return response
